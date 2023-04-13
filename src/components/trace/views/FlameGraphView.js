40a657b417cf56e48a817a30e0c52e4e6e034543
Mehmet Emin INAC <mehmetemininac@gmail.com>
2023-04-14 00:18:52 +0200

    Transform data to flamegraph format

--- src/components/trace/views/FlameGraphView.js
+++ src/components/trace/views/FlameGraphView.js
@@ -3,21 +3,59 @@ import "./../../Trace.css"
 import TraceCard from "./../TraceCard"
 import FlameGraph from "./FlameGraph"
 
+const comparaSpans = (a, b) => {
+  if(`${a.self_class} ${a.method_name}` < `${b.self_class} ${b.method_name}`) {
+    return -1;
+  } else if(`${a.self_class} ${a.method_name}` > `${b.self_class} ${b.method_name}`) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+const mergeSpans = (spans) => {
+  const sortedSpans = spans.sort(comparaSpans);
+  const mergedSpans = [];
+
+  for(let i = 0; i < sortedSpans.length; i++) {
+    let current = sortedSpans[i];
+
+    for(let j = i + 1; j < sortedSpans.length; j++) {
+      if(comparaSpans(current, sortedSpans[j]) == 0) {
+        current.duration += sortedSpans[j].duration;
+        current.spans = current.spans.concat(sortedSpans[j].spans);
+
+        i = j; // so it does not pass through same item.
+      } else {
+        break;
+      }
+    }
+
+    mergedSpans.push(current);
+  }
+
+  return mergedSpans;
+};
+
 const flameGraphData = (spans) => {
   if(spans.length == 0) return [];
 
-  return spans.flatMap((span) => {
+  return mergeSpans(spans).flatMap((span) => {
     const children = flameGraphData(span.spans);
-    const data = { name: `${span.defined_class} ${span.method_name}`, value: 1, children: children };
+    const data = { name: `${span.self_class} ${span.method_name}`, value: span.duration, children: children };
 
     return data;
   });
 }
 
-const FlameGraphView = ({ trace, goBack }) => {
-  const data = { children: flameGraphData(trace.trace.spans) };
+const totalDuration = (trace) => {
+  return trace.spans.reduce((sum, span) => {
+    return sum + span.duration
+  }, 0);
+};
 
-  console.log(data);
+const FlameGraphView = ({ trace, goBack }) => {
+  const data = { children: flameGraphData(trace.trace.spans), name: "root", value: totalDuration(trace.trace) };
 
   return (
     <div>
